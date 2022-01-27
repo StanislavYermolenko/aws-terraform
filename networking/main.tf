a814804b3235441eb21fc5806a1e1b6d08854221
StanislavYermolenko <stanislav.yermolenko@gmail.com>
2022-01-27 13:36:03 +0000

    add the cidrsubnet function

--- networking/main.tf
+++ networking/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "mtc_vps" {
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-  count = length(var.public_cidrs)
+  count = var.public_sn_count
   vpc_id = aws_vpc.mtc_vps.id
   cidr_block = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
@@ -28,7 +28,7 @@ resource "aws_subnet" "mtc_public_subnet" {
 }
   
 resource "aws_subnet" "mtc_private_subnet" {
-  count = length(var.private_cidrs)
+  count = var.private_sn_count
   vpc_id = aws_vpc.mtc_vps.id
   cidr_block = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
--- networking/variables.tf
+++ networking/variables.tf
@@ -11,3 +11,11 @@ variable "public_cidrs" {
 variable "private_cidrs" {
     type = list
 }
+
+variable "public_sn_count" {
+    type = number
+}
+
+variable "private_sn_count" {
+    type = number
+}
